1d5e24f1f346251ad156d3e00df5c333c497ad3a
nurgul <NurgulDev>
2024-04-16 21:21:46 +0600

    Header in Main project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import './App.css'
+import "./App.css";
+import HeaderMainComponents from "./components/MainComponents/HeaderMainComponents/HeaderMainComponents";
+import HeaderMainComponentsSecend from "./components/MainComponents/HeaderMainComponentsSecend/HeaderMainComponentsSecend";
+import MainComponentsBlock from "./components/MainComponents/MainComponentsBlock/MainComponentsBlock";
 
 function App() {
-	return <div></div>
+  return (
+    <div>
+      <HeaderMainComponents />
+      <HeaderMainComponentsSecend />
+      <MainComponentsBlock />
+    </div>
+  );
 }
 
-export default App
+export default App;
